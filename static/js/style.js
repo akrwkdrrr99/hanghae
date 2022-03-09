c984da88b03f9ebb92bc9ad7dfe32d70648bc1ad
DESKTOP-O3U018O\Hyunggi <phg9587@naver.com>
2022-03-09 09:22:20 +0900

    - 페이지 일부 뼈대 완성

--- static/js/style.js
+++ static/js/style.js
@@ -13,3 +13,33 @@ document.addEventListener("DOMContentLoaded", function(){
         pw_find_box.classList.toggle('on');
     })
 });
+
+function posting() {
+    let user_ID = $('#user_ID').val()
+    let url = $('#url').val()
+    let star = $('#star').val()
+    let comment = $('#comment').val()
+
+    if(star == "-- 선택하기 --"){
+        alert("평점을 선택해주세요")
+        return
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: '/api',
+        data: {reqType : 'postMovieData',user_ID_give : user_ID ,url_give: url, star_give: star, comment_give:comment},
+        success: function (response) {
+            alert(response['msg'])
+            // window.location.reload()
+        }
+    });
+}
+
+function enter_board_write() {
+    window.location.href="/board_write"
+}
+
+function close_box() {
+    window.location.href="/"
+}
