31ea98d4870febbcb3223aa18f9d574c4d26fc16
DESKTOP-O3U018O\Hyunggi <phg9587@naver.com>
2022-03-09 10:25:51 +0900

    기능 추가 1. board_write 에서 영화 등록시 빈칸이 있으면 알람 2. URL에 네이버 영화 주소가 아닌 경우 알람

--- static/js/style.js
+++ static/js/style.js
@@ -20,8 +20,29 @@ function posting() {
     let star = $('#star').val()
     let comment = $('#comment').val()
 
-    if(star == "-- 선택하기 --"){
+    if(user_ID == ""){
+        alert("ID를 입력해주세요")
+        return
+    }
+    else if(url == ""){
+        alert("영화URL을 입력해주세요")
+        return
+    }
+    else if(star == "-- 선택하기 --"){
         alert("평점을 선택해주세요")
+        return
+    }
+    else if(comment == ""){
+        alert("추천 이유를 입력해주세요")
+        return
+    }
+
+    // indexOf : 해당 문자열로 시작 하면 시작하는 index값을 return, 없으면 -1 return
+    if (url.indexOf("https://movie.naver.com/movie/bi/mi/basic.naver?code=") >= 0) {
+        // alert("옳은 주소 입니다.")
+    }
+    else{
+        alert("잘못된 주소 입니다. 네이버 영화에서 영화를 검색해주세요. (＃https:// 붙여주세요.)")
         return
     }
 
