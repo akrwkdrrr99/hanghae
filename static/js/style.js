f4957ece9aebc8ff5e497f95322d2452464ae4a7
hakseon-yoo <gkrtjs2020@naver.com>
2022-03-10 09:19:51 +0900

    2022. 3. 10. hsyoo. style.js에 사용하지 않는 소스 제거

--- static/js/style.js
+++ static/js/style.js
@@ -167,26 +167,6 @@ function move_board_detail(index) {
     }
 }
 
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 function serch_btn() {
     let condition = $('#serch_condition').val()
     let serch_text = $('#serch_text_input_box').val()
